302634902b1f2e37f2f4d50a9ff1795cac80e2fd
Nathália Zuza <nathalia555zuza@gmail.com>
2023-01-03 17:35:05 -0300

    modules

---
+++ NodeEstudos/app.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./soma")
+var subFunc = require("./sub")
+var divFunc = require("./div")
+var multFunc = require("./mult")
+
+console.log("Soma: " + somaFunc(5,4))
+console.log("Subtração: " + subFunc(5,4))
+console.log("Divisão: " + divFunc(5,4))
+console.log("Multiplicação: " + multFunc(5,4))
